2a303da4acfb9dedfa1e6aa29636dbfd1a662757
luk <lovemomoco@gmail.com>
2016-01-22 10:46:30 +0800

    update demo

--- script.js
+++ script.js
@@ -19,12 +19,44 @@ var g = function(id){
 
 //3.添加幻灯片的操作（所有幻灯片&对应的按钮）
 function addSliders(){
-  //3.1获取模板
+    //3.1获取模板
     var tpl_main = g('template_main').innerHTML.replace(/^\s*/,'').replace(/\s*$/,'');
     var tpl_ctrl = g('template_ctrl').innerHTML.replace(/^\s*/,'').replace(/\s*$/,'');
+    //3.2定义最终输出HTML的变量
+    var out_main = [];
+    var out_ctrl = [];
+    //3.3遍历所有数据，构建最终输出的HTML
+    for(i in data){
+        var _html_main = tpl_main.replace(/{{index}}/g,data[i].img).replace(/{{h2}}/g,data[i].h2).replace(/{{h3}}/g,data[i].h3);
+        var _html_ctrl = tpl_ctrl.replace(/{{index}}/g,data[i].img);
+        out_main.push(_html_main);
+        out_ctrl.push(_html_ctrl);
+    }
+    //3.4把HTML回写到对应的DOM里面
+    g('template_main').innerHTML = out_main.join('');
+    g('template_ctrl').innerHTML = out_ctrl.join('');
+}
+//5.幻灯片切换
+function switchSlider(n){
+    //5.1获得要展现的幻灯片&控制按钮DOM
+    var main = g('main_'+n);
+    var ctrl = g('ctrl_'+n);
+
+    //5.2获得所有的幻灯片以及控制按钮
+    var clear_main = g('.main-i');
+    var clear_ctrl = g('.ctrl-i');
+    //5.3清除他们的active样式
+    for(i=0;i<clear_ctrl.length;i++){
+        clear_main[i].className = clear_main[i].className.replace(' main-i_active','');
+        clear_ctrl[i].className = clear_ctrl[i].className.replace(' ctrl-i_active','');
+    }
+    //5.4为当前控制按钮和幻灯片附加样式
+    main.className += ' main-i_active';
+    ctrl.className += ' ctrl-i_active';
 }
 
 //4.定义何时处理幻灯片输出
 window.onload = function(){
     addSliders();
+    switchSlider(1);
 };
